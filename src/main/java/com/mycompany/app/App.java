cbbc78aa13f333d470c123827dc4fea0f627faa9
avo-is-coding <elynnho1999@gmail.com>
2025-04-09 16:14:42 +0800

    My first change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Spelling is fun!");
+        System.out.println("2nd commit!");
     }
 
     public String getMessage() {
